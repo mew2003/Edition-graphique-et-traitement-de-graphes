478d8829fe41c76f90165f3c0ac4206852020bf5
ReDhix <Sebastien.dasilvaoliveira@gmail.com>
2022-12-08 15:18:22 +0100

    Ajout de l'option pour changer de scène
    lezgoooooo

--- JavaFXApplication7/src/javafxapplication7/Main.java
+++ JavaFXApplication7/src/javafxapplication7/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
