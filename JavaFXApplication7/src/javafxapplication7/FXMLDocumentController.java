cef093011f659d90bfebaf41e766dbf6e6cc7e6d
ReDhix <Sebastien.dasilvaoliveira@gmail.com>
2023-01-06 08:37:30 +0100

    Fonction pour surligner les arcs avec le menu dans un graphe orienté fonctionnelle
    On peut maintenant surligner tous les liens, tous les noeuds, toutes les boucles dans un graphe orienté et non orienté.
    
    On peut également changer le lien de noeuds à l'aide de la souris et créer des boucles dans un graphe orienté.
    
    TODO : mettre des commentaires et optimiser les méthodes (voir  méthodes pour changer les liens)

--- JavaFXApplication7/src/javafxapplication7/FXMLDocumentController.java
+++ JavaFXApplication7/src/javafxapplication7/FXMLDocumentController.java
@@ -6,6 +6,7 @@ package javafxapplication7;
 import java.awt.Event;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutionException;
 import java.util.spi.LocaleNameProvider;
 
 import app.FactoryGraphe;
@@ -379,7 +380,16 @@ public class FXMLDocumentController implements Initializable {
                     						}
                     					}
                         				if(ok) {
-                        					lien.setNoeuds(noeuds, zoneDessin);
+                        					for(int i = 0; i < listeElements.getItems().size() ; i++) {
+                        						String test = listeElements.getItems().get(i).toString();
+                        						if(test.equals(lien.toString())) {
+                        							listeElements.getItems().remove(i);
+                        						}		
+                        					}
+                        					graphe.supprimerLien(lien, zoneDessin);
+                        					Lien nouveauLien = graphe.creerLien(noeuds[0], noeuds[1]);
+                        					nouveauLien.dessiner(zoneDessin);
+                        					listeElements.getItems().addAll(nouveauLien);
                         				}
                     				}
                     			});
@@ -407,14 +417,24 @@ public class FXMLDocumentController implements Initializable {
                     					Noeud noeud = (Noeud) graphe.elementClicked(pos, zoneDessin);
                     					Noeud[] noeuds = {link.getNoeuds()[0], noeud};
                     					boolean ok = true;
+                    					
                     					for(int i = 0; i < listeElements.getItems().size() ; i++) {
                     						String test = listeElements.getItems().get(i).toString();
                     						if(test.equals("Lien : [" + noeuds[0].getNom() + ", " + noeuds[1].getNom() + "]")) {
                     							ok = false;
                     						}
                     					}
-                        				if(ok) {
-                        					lien.setNoeuds(noeuds, zoneDessin);
+                        				if(ok && !(noeuds[0].equals(noeud))) {
+                        					for(int i = 0; i < listeElements.getItems().size() ; i++) {
+                        						String test = listeElements.getItems().get(i).toString();
+                        						if(test.equals(lien.toString())) {
+                        							listeElements.getItems().remove(i);
+                        						}		
+                        					}
+                        					graphe.supprimerLien(lien, zoneDessin);
+                        					Lien nouveauLien = graphe.creerLien(noeuds[0], noeuds[1]);
+                        					nouveauLien.dessiner(zoneDessin);
+                        					listeElements.getItems().addAll(nouveauLien);
                         				}
                     				}
                     			});
@@ -584,12 +604,14 @@ public class FXMLDocumentController implements Initializable {
             } else if(link instanceof LienOriente) {
             	LienOriente lienOR = (LienOriente) link;
             	graphe.reset();
-            	for(Line l : lienOR.getLine()) {
-            		l.setStrokeWidth(3.0);
-            	}
-            	for(Shape shape : lienOR.getArc()) {
-            		shape.setStrokeWidth(3.0);
-            	}
+            	try {
+            		for(Shape shape : lienOR.getArc()) {
+            			shape.setStrokeWidth(3.0);
+            		}
+            	} catch (NullPointerException e) {}
+            	for(Line line : lienOR.getLine()) {
+        			line.setStrokeWidth(3.0);
+        		}	
             }            
     	} catch (Exception e) {}
     	
